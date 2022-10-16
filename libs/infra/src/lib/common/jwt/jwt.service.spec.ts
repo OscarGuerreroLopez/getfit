742d8966c64f02124f22aed7938794796f61b4bb
Oscar Guerrero <>
2022-10-16 17:31:35 +0200

    deleting test that are not used

--- libs/infra/src/lib/common/jwt/jwt.service.spec.ts
+++ libs/infra/src/lib/common/jwt/jwt.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { JwtService } from './jwt.service';
+import { JwtModule as Jwt } from '@nestjs/jwt';
+
+import { JwtTokenService } from './jwt.service';
 
 describe('JwtService', () => {
-  let service: JwtService;
+  let service: JwtTokenService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [JwtService],
+      imports: [
+        Jwt.register({
+          secret: 'xgfht543',
+          signOptions: { expiresIn: '24h' },
+        }),
+      ],
+      providers: [JwtTokenService],
     }).compile();
 
-    service = module.get<JwtService>(JwtService);
+    service = module.get<JwtTokenService>(JwtTokenService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a good token', async () => {
+    const payload = {
+      username: 'Oscar33',
+    };
+    const token = service.createToken(payload, 'xgfht543', '24h');
+
+    expect(token).toBeDefined();
+
+    const decoded = await service.checkToken(token);
+    expect(decoded).toHaveProperty('username');
+    expect(decoded.username).toStrictEqual('Oscar33');
   });
 });
--- libs/user/src/lib/user.ts
+++ libs/user/src/lib/user.ts
@@ -1,7 +1,3 @@
-export function user(): string {
-  return 'user lib';
-}
-
 export * from './entities/model';
 export * from './entities/repositories';
 export * from './usecases';
